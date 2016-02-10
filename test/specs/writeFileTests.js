afd656ebf19053c98a3c8aa84d10818a33c6b0e5
Perry Mitchell <perry@perrymitchell.net>
2016-02-10 22:00:32 +0200

    Added a writeFile test

---
+++ test/specs/writeFileTests.js
@@ -0,0 +1,62 @@
+(function(module) {
+
+	"use strict";
+
+	var webdavfs = require(__dirname + "/../../source/index.js"),
+		wfs = webdavfs("http://localhost:9999/"),
+        fs = require("fs"),
+        isThere = require("is-there");
+
+	var jsDAV = require("jsDAV/lib/jsdav"),
+		jsDAV_Locks_Backend_FS = require("jsDAV/lib/DAV/plugins/locks/fs");
+
+	module.exports = {
+
+		setUp: function(done) {
+            this.testFileName = __dirname + "/../resources/test.dat";
+			this.server = jsDAV.createServer({
+				node: __dirname + "/../resources/",
+				locksBackend: jsDAV_Locks_Backend_FS.new(__dirname + "/data")
+			}, 9999);
+			setTimeout(done, 250);
+		},
+
+		tearDown: function(done) {
+            var targetFile = this.testFileName;
+			this.server.close(function() {
+				if (isThere(targetFile)) {
+					fs.unlinkSync(targetFile);
+				}
+				setTimeout(done, 100);
+			});
+		},
+
+		writeFile: {
+
+            testWriteData: function(test) {
+                var buffer = new Buffer(4),
+                    targetFile = this.testFileName;
+                buffer.writeFloatBE(0xcafebabe, 0);
+                wfs.writeFile("test.dat", buffer, function(err) {
+                    if (err) {
+                        throw err;
+                    }
+                    var newBuffer = new Buffer(4);
+                    fs.open(targetFile, 'r', function(openerr, fd) {
+                        if (openerr) {
+                            throw openerr;
+                        }
+                        fs.read(fd, newBuffer, 0, 4, 0, function(err, num) {
+                            test.ok(buffer.equals(newBuffer), "Read data should match written");
+                            fs.closeSync(fd);
+                            test.done();
+                        });
+                    });
+                });
+            }
+
+		}
+
+	};
+
+})(module);
