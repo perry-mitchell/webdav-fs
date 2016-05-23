38f93d5fc393febff8184dd378a728ef299fa08e
bojan88 <bojan88@gmail.com>
2016-05-23 18:08:03 +0200

    Indentation - Issue #20

--- test/specs/brototypeTests.js
+++ test/specs/brototypeTests.js
@@ -1,57 +1,57 @@
 (function(module) {
 
-	"use strict";
+    "use strict";
 
-	var Bro = require(__dirname + "/../../source/brototype.js");
+    var Bro = require(__dirname + "/../../source/brototype.js");
 
-	module.exports = {
+    module.exports = {
 
-		setUp: function(done) {
-			this.bro = Bro({
-				one: {
-					small: {
-						thing: 8
-					}
-				}
-			});
-			(done)();
-		},
+        setUp: function(done) {
+            this.bro = Bro({
+                one: {
+                    small: {
+                        thing: 8
+                    }
+                }
+            });
+            (done)();
+        },
 
-		iCanHaz1: {
+        iCanHaz1: {
 
-			testSingleExisting: function(test) {
-				test.strictEqual(this.bro.iCanHaz1("one.small.thing"), 8);
-				test.done();
-			},
+            testSingleExisting: function(test) {
+                test.strictEqual(this.bro.iCanHaz1("one.small.thing"), 8);
+                test.done();
+            },
 
-			testSingleNonExisting: function(test) {
-				test.strictEqual(this.bro.iCanHaz1("one.small.thingy"), undefined);
-				test.done();
-			},
+            testSingleNonExisting: function(test) {
+                test.strictEqual(this.bro.iCanHaz1("one.small.thingy"), undefined);
+                test.done();
+            },
 
-			testMultiLastExisting: function(test) {
-				test.strictEqual(this.bro.iCanHaz1(
-					"two.small.thing", "one.small.thing"
-				), 8);
-				test.done();
-			},
+            testMultiLastExisting: function(test) {
+                test.strictEqual(this.bro.iCanHaz1(
+                    "two.small.thing", "one.small.thing"
+                ), 8);
+                test.done();
+            },
 
-			testMultiFirstExisting: function(test) {
-				test.strictEqual(this.bro.iCanHaz1(
-					"one.small.thing", "two.small.thing"
-				), 8);
-				test.done();
-			},
+            testMultiFirstExisting: function(test) {
+                test.strictEqual(this.bro.iCanHaz1(
+                    "one.small.thing", "two.small.thing"
+                ), 8);
+                test.done();
+            },
 
-			testMultiNoneExisting: function(test) {
-				test.strictEqual(this.bro.iCanHaz1(
-					"one.small.thingy", "two.small.thing"
-				), undefined);
-				test.done();
-			}
+            testMultiNoneExisting: function(test) {
+                test.strictEqual(this.bro.iCanHaz1(
+                    "one.small.thingy", "two.small.thing"
+                ), undefined);
+                test.done();
+            }
 
-		}
+        }
 
-	};
+    };
 
 })(module);
