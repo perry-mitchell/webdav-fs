780a88a2c5cc8e944b567494ebde17d19d74d8c3
Perry Mitchell <perry@perrymitchell.net>
2016-07-13 22:00:32 +0300

    Fixed indentation and removed module wrappers - fixes #24

--- source/processing.js
+++ source/processing.js
@@ -1,22 +1,18 @@
-(function(module) {
+"use strict";
 
-	"use strict";
+module.exports = {
 
-	module.exports = {
+	createStat: function(itemInfo) {
+		return {
+			isFile: function() {
+				return itemInfo.type === "file";
+			},
+			isDirectory: function() {
+				return itemInfo.type === "directory";
+			},
+			mtime: new Date(itemInfo.lastmod),
+			size: itemInfo.size || 0
+		};
+	}
 
-		createStat: function(itemInfo) {
-			return {
-				isFile: function() {
-					return itemInfo.type === "file";
-				},
-				isDirectory: function() {
-					return itemInfo.type === "directory";
-				},
-				mtime: new Date(itemInfo.lastmod),
-				size: itemInfo.size || 0
-			};
-		}
-
-	};
-
-})(module);
+};
