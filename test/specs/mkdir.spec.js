4c35bc8f676cfc90ce1558cffed1908dcdc5599d
Perry Mitchell <perry@perrymitchell.net>
2017-08-07 18:17:39 +0300

    Setup new testing system

---
+++ test/specs/mkdir.spec.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const directoryExists = require("directory-exists").sync;
+
+const targetDir = path.resolve(__dirname, "../testContents/sub folder/new");
+
+describe("mkdir", function() {
+
+    beforeEach(function() {
+        setup.call(this);
+    });
+
+    afterEach(function() {
+        tearDown.call(this);
+    });
+
+    it("creates a directory", function(done) {
+        expect(directoryExists(targetDir)).to.be.false;
+        this.client.mkdir("/sub folder/new", (err) => {
+            expect(err).to.be.null;
+            expect(directoryExists(targetDir)).to.be.true;
+            done();
+        });
+    });
+
+    it("throws if directory already exists", function(done) {
+        this.client.mkdir("/sub folder", (err) => {
+            expect(err.message).to.match(/Method Not Allowed/i);
+            done();
+        });
+    });
+
+});
