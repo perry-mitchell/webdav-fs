507b14a692e10302c370dccfc26802f6885ccac8
Perry Mitchell <perry@perrymitchell.net>
2018-03-25 22:39:05 +0300

    Add test for trailing slashes in readdir requests

--- test/specs/readdir.spec.js
+++ test/specs/readdir.spec.js
@@ -27,7 +27,6 @@ describe("readdir", function() {
         });
     });
 
-    // This should work in the future:
     it("reads the contents of a directory with non-latin characters", function(done) {
         this.client.readdir("/방각하éàöåçΘΣฐ", (err, contents) => {
             expect(err).to.be.null;
@@ -39,6 +38,14 @@ describe("readdir", function() {
     it("throws an error if the directory doesn't exist", function(done) {
         this.client.readdir("/non-existent", (err, contents) => {
             expect(err.message).to.match(/Not Found/i);
+            done();
+        });
+    });
+
+    it("returns only the expected contents when using trailing slashes (#56)", function(done) {
+        this.client.readdir("/dir1/", (err, contents) => {
+            expect(err).to.be.null;
+            expect(contents).to.deep.equal(["dir2"]);
             done();
         });
     });
